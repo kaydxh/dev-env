922cdd11466f997c2731ffbf7ac90b39f71cba3f
kaydxh <xiao_hua_ding@sina.cn>
2020-09-06 23:20:48 +0800

    [CHG] change docker params

--- docker/docker-image/centos7/build-container.sh
+++ docker/docker-image/centos7/build-container.sh
@@ -5,4 +5,4 @@
 docker build -t kayxhding/centos7 .
 
 #build container
-docker run --privileged --security-opt seccomp=unconfined -net=host -it --name dev-centos7  -v ~/workspace:/Users/dingxiaohua/workspace -v ~/Enviroment/gopath:/Users/dingxiaohua/workspace/gopath -v ~/Enviroment/lib:/usr/local -v ~/Downloads:/Users/dingxiaohua/Downloads kayxhding/centos7 /bin/bash
+docker run --privileged --security-opt seccomp=unconfined -net=host -it --name dev-centos7  -v ~/workspace:/Users/kayxhding/workspace -v ~/Enviroment/gopath:/Users/kayxhding/workspace/gopath -v ~/Enviroment/lib:/usr/local -v ~/Downloads:/Users/kayxhding/Downloads kayxhding/centos7 /bin/bash
